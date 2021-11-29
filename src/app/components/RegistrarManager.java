aeeb2dc6f7660c6a64990f049937d16e50af8e8b
Pinglim11 <filip.lim@obf.ateneo.edu>
2021-11-29 23:41:52 +0800

    modified register manager

--- src/app/components/RegistrarManager.java
+++ src/app/components/RegistrarManager.java
@@ -1,6 +1,24 @@
 package app.components;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import app.repository.HoldOrderRepository;
+
+@Component
 public class RegistrarManager {
 	
+	@Autowired 
+	private HoldOrderRepository horepo;
+	
 
+	
+	public HoldOrderDto save(HoldOrderDto hold) {
+		
+		
+		
+		
+		
+		return (hold);
+	}
 }
