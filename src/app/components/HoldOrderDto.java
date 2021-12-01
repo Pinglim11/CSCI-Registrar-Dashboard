de7a8fb2919d3e29345f0a8b03bcd1ac919897e0
Pinglim11 <filip.lim@obf.ateneo.edu>
2021-12-01 12:59:32 +0800

    registrar manager

--- src/app/components/HoldOrderDto.java
+++ src/app/components/HoldOrderDto.java
@@ -1,5 +1,63 @@
 package app.components;
 
 public class HoldOrderDto {
-
+	
+	private Long idnumber;
+	private String surname;
+	private String firstname;
+	private String middleinitial;
+	private Integer yearlevel;
+	private String course;
+	private String email;
+	
+	
+	public Long getIdnumber() {
+		return idnumber;
+	}
+	public void setIdnumber(Long idnumber) {
+		this.idnumber = idnumber;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getMiddleinitial() {
+		return middleinitial;
+	}
+	public void setMiddleinitial(String middleinitial) {
+		this.middleinitial = middleinitial;
+	}
+	public Integer getYearlevel() {
+		return yearlevel;
+	}
+	public void setYearlevel(Integer yearlevel) {
+		this.yearlevel = yearlevel;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "HoldOrderDto [idnumber=" + idnumber + ", surname=" + surname + ", firstname=" + firstname
+				+ ", middleinitial=" + middleinitial + ", yearlevel=" + yearlevel + ", course=" + course + ", email="
+				+ email + "]";
+	}
+	
 }
--- src/app/components/RegistrarManager.java
+++ src/app/components/RegistrarManager.java
@@ -3,7 +3,10 @@ package app.components;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import app.entity.Student;
 import app.repository.HoldOrderRepository;
+import app.repository.PaymentRepository;
+import app.repository.StudentRepository;
 
 @Component
 public class RegistrarManager {
@@ -11,10 +14,24 @@ public class RegistrarManager {
 	@Autowired 
 	private HoldOrderRepository horepo;
 	
+	@Autowired
+	private StudentRepository studentrepo;
+	
+	@Autowired
+	private PaymentRepository paymentrepo;
 
 	
+	
 	public HoldOrderDto save(HoldOrderDto hold) {
-		
+		Student student = new Student();
+		student.setIdnumber(hold.getIdnumber());
+		student.setSurname(hold.getSurname());
+		student.setFirstname(hold.getFirstname());
+		student.setMiddleinitial(hold.getMiddleinitial());
+		student.setYearlevel(hold.getYearlevel());
+		student.setCourse(hold.getCourse());
+		student.setEmail(hold.getEmail());
+		student = studentrepo.save(student);
 		
 		
 		
